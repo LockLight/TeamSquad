1d89d0bc13ee4a7a5dafc792be5bdad88a12d357
locklight <locklightz@gmail.com>
2017-12-27 11:22:47 +0800

    Initial Commit

---
+++ TeamSquad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TeamSquad
+//
+//  Created by locklight on 2017/12/27.
+//  Copyright © 2017年 locklight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TeamSquad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TeamSquad
+//
+//  Created by locklight on 2017/12/27.
+//  Copyright © 2017年 locklight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
